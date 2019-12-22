a39477403e035458834787f16e13504c79c98fad
filipeandrev <filipe.andrev7@gmail.com>
2019-12-22 10:40:44 -0300

    GH-12 Subindo migration de funcionarios

---
+++ projeto/database/migrations/2019_12_22_125210_create_funcionarios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('cpf')->unique();
+            $table->string('siape')->unique();
+            $table->string('setor');
+            $table->string('cargo');
+            $table->string('telefone');
+            $table->string('email');
+            $table->string('senha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+}
