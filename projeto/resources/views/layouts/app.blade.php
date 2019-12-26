bf1d57e653b05796472592baf58501d22d17d3a6
GuilhermeH6 <guilhermeholanda870@gmail.com>
2019-12-25 23:50:36 -0300

    GH-2 pagina de welcome

--- projeto/resources/views/layouts/app.blade.php
+++ projeto/resources/views/layouts/app.blade.php
@@ -8,7 +8,13 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
     <title>IF Parking</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/">
 
+<!-- Bootstrap core CSS -->
+<link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="justified-nav.css" rel="stylesheet">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -35,6 +41,7 @@
 <main>
     @yield('content')
 </main>
+
 <footer class="mt-5 bg-dark">
   <div class="container pt-4">
       <div class="row">
--- projeto/resources/views/welcome.blade.php
+++ projeto/resources/views/welcome.blade.php
@@ -2,6 +2,37 @@
 
 @section('content')
 
-<h1 class="display-5 text-center mt-5">IF Parking</h1>
+<div class="container">
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>IF Parking</h1>
+        <p class="lead">Este sistema tem a finalidade de servir como um controle de entrada e saída de veículos, para o acesso ao estacionamento do novo campus Igarassu.</p>
+        <p><a class="btn btn-lg btn-success btn-dark" href="{{ route('register') }}" role="button">Cadastre-se!</a></p>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>Cadastre seu carro</h2>
+          <p>Alunos e professores podem cadastrar seus veículos.</p>
+          <p><a class="btn btn-primary btn-dark" href="#" role="button">Mais detalhes &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+          <h2>Controle de vagas</h2>
+          <p>Saiba das vagas disponiveis no estacionamento.</p>
+          <p><a class="btn btn-primary btn-dark" href="#" role="button">Mais detalhes &raquo;</a></p>
+       </div>
+        <div class="col-lg-4">
+          <h2>Um ambiente mais organizado</h2>
+          <p>Com o controle de entrada e saída de veículos a organização do local fica bem mais acessivel.</p>
+          <p><a class="btn btn-primary btn-dark" href="#" role="button">Mais detalhes &raquo;</a></p>
+        </div>
+      </div>
+    </div>
+
+
+  </body>
+</html>
 
 @endsection
