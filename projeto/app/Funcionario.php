dd314dd3ba086e215972e5e82fe3ae4084b4cd44
Aline Venceslau <alineepecris79@gmail.com>
2019-12-28 04:02:54 -0300

    GH-16 mudando campo de senha para password

--- projeto/app/Funcionario.php
+++ projeto/app/Funcionario.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Funcionario extends Model
 {
-    protected $fillable = ['nome', 'cpf', 'siape', 'setor', 'cargo', 'telefone', 'email', 'senha'];
+    protected $fillable = ['nome', 'cpf', 'siape', 'setor', 'cargo', 'telefone', 'email', 'password'];
     protected $table = 'funcionarios';
 }
