f305a4e65203e23a917d04dda5f55cbf75cd24f5
GuilhermeH6 <guilhermeholanda870@gmail.com>
2019-12-27 22:59:58 -0300

    GH-2 Ajustando as cores do site

--- projeto/resources/views/layouts/app.blade.php
+++ projeto/resources/views/layouts/app.blade.php
@@ -21,14 +21,14 @@
 
 <body>
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light border border-left rounded">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border border-left rounded ">
   <a class="navbar-brand" href="{{ url('/') }}">IF Parking</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="collapse navbar-collapse ">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="{{ url('/login') }}">Entrar</a>
+    <a class="btn btn-md btn-success btn-success" href="{{ url('/login') }}">Entrar</a>
     </div>
   </div>
         </nav>
@@ -38,7 +38,7 @@
     @yield('content')
 </main>
 
-<footer class="py-4 bg-dark text-white-50">
+<footer class="py-4 bg-success text-black-50">
     <div class="container text-center">
       <small>IF PARKING &copy; 2020</small>
     </div>
