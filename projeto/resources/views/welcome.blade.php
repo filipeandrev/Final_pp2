89cb59635d862ced468122e6dc9392dc01a2b711
GuilhermeH6 <guilhermeholanda870@gmail.com>
2019-12-28 18:38:42 -0300

    GH-2 Adicionando carrossel

--- projeto/resources/views/welcome.blade.php
+++ projeto/resources/views/welcome.blade.php
@@ -11,22 +11,22 @@
         <p><a class="btn btn-lg btn-success btn-success" href="{{ route('register') }}" role="button">Cadastre-se!</a></p>
       </div>
 
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-lg-4">
-          <h2>Cadastre seu carro</h2>
-          <p>Alunos e professores podem cadastrar seus veículos.</p>
-        </div>
-        <div class="col-lg-4">
-          <h2>Controle de vagas</h2>
-          <p>Saiba das vagas disponiveis no estacionamento.</p>
-       </div>
-        <div class="col-lg-4">
-          <h2>Um ambiente mais organizado</h2>
-          <p>Com o controle de entrada e saída de veículos a organização do local fica bem mais acessivel.</p>
-        </div>
-      </div>
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <h1>Cadastre seu carro</h1>
+      <h5>Alunos e professores podem cadastrar seus veículos no estacinamento.</h5>
     </div>
+    <div class="carousel-item">
+      <h1>Controle de vagas</h1>
+      <h5>Fique por dentro das vagas disponiveis no estacionamento.</h5>
+    </div>
+    <div class="carousel-item">
+      <h1>Um ambiente mais organizado</h1>
+      <h5>Com o controle de entrada e saída de veículos a organização do local fica bem mais acessivel.</h5>
+    </div>
+  </div>
+</div>
   </body>
 </html>
 
