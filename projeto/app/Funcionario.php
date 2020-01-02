9737c631e43c819301b67215c70c8b7ba10c0c23
Aline Venceslau <alineepecris79@gmail.com>
2020-01-02 03:24:58 -0300

    GH-16 ajustando model de funcionarios

--- projeto/app/Funcionario.php
+++ projeto/app/Funcionario.php
@@ -3,9 +3,33 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticable;
 
-class Funcionario extends Model
+
+class Funcionario extends Authenticable
 {
-    protected $fillable = ['nome', 'cpf', 'siape', 'setor', 'cargo', 'telefone', 'email', 'password'];
-    protected $table = 'funcionarios';
+	protected $table = 'funcionarios';
+
+	protected $guard = 'funcionario';
+
+    protected $fillable = [
+    	'nome',
+    	'cpf',
+    	'siape', 
+    	'setor', 
+    	'cargo', 
+    	'telefone', 
+    	'email', 
+    	'password'];
+
+    protected $hidden = [
+    	'password'
+    ];	
+
+    protected $guarded = [
+
+    	'id',
+    	'created_at',
+    	'update_at'
+    ];
 }
