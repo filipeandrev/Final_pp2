4565fa96081fbc2a78aaf28ebd66e105dbbc55d5
filipeandrev <filipe.andrev7@gmail.com>
2020-01-02 08:36:22 -0300

    GH-25 Adicionando relaçao com ostras models

--- projeto/app/Veiculo.php
+++ projeto/app/Veiculo.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Funcionario;
+use App\Aluno;
 
 class Veiculo extends Model
 {
+    protected $table = 'veiculos';
     protected $fillable = ['tipo'];
 }
