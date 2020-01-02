2afed3c16bdeabd08724d46f33d06b16093a69d2
Aline Venceslau <alineepecris79@gmail.com>
2020-01-02 03:43:01 -0300

    GH-23 criando form de login de funcionario

--- projeto/resources/views/auth/login.blade.php
+++ projeto/resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="card-body">
 
-                     
+                    
 
                     <div class = 'alu'>
                     <form method="post" action="{{ route('login') }}">
@@ -73,7 +73,67 @@
                 </div>
 
 
-                
+                <div class = 'func'>
+                <form method="post" action="{{ route('log') }}">
+                        @csrf
+                        
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">                                   
+
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-success">
+                                    {{ __('Entrar') }}
+                                </button>
+
+                            
+                                <hr width="50%" align="left">
+                                <div>
+ 
+                                    <p>Não tem registro?
+                                    <a class="btn btn-link" href="{{ route('register') }}">
+                                        {{ __('Cadastre-se') }}
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
   
        
             </div>
